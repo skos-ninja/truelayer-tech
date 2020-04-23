5fc5fdb2c1acd77db591217fb845910e50edacc4
Jake Oliver <github@skos.ninja>
2020-04-23 10:34:06 +0100

    Add top level command

---
+++ cmd/cmd.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/spf13/cobra"
+)
+
+var root = &cobra.Command{
+	Use: "truelayer",
+}
+
+func init() {
+	// Register a flag for all commands that allows setting the http port
+	root.PersistentFlags().Int("port", 8080, "Port to listen on")
+
+	// Add sub commands here
+}
+
+func main() {
+	if err := root.Execute(); err != nil {
+		panic(err)
+	}
+}
