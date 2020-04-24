1bfa4965e8034142a9325deb5fdc1ff978dda25f
Jake Oliver <github@skos.ninja>
2020-04-24 12:34:23 +0100

    Remove non used exports

--- svc/pokemon/app/app.go
+++ svc/pokemon/app/app.go
@@ -4,16 +4,12 @@ import (
 	"context"
 
 	"github.com/skos-ninja/truelayer-tech/svc/pokemon/services/pokeapi"
-	"github.com/skos-ninja/truelayer-tech/svc/pokemon/services/pokeapi/models"
 	"github.com/skos-ninja/truelayer-tech/svc/pokemon/services/shakespeare"
 
 	"github.com/hashicorp/golang-lru/simplelru"
 )
 
 type App interface {
-	GetPokemonSpecies(ctx context.Context, pokemon string) (*models.PokemonSpecies, error)
-	GetShakespeareText(ctx context.Context, text string) (string, error)
-
 	GetShakespearePokemonDescription(ctx context.Context, pokemon string) (string, error)
 }
 
